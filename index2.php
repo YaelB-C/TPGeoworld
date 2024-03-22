370842fb6c1ac17ea1adbebe420db9eb5b101f0e
YaelB-C <157603976+YaelB-C@users.noreply.github.com>
2024-03-22 09:39:39 +0100

    Add files via upload

--- index2.php
+++ index2.php
@@ -31,7 +31,7 @@ $desPays = getCountriesByContinent($continent);
   <div class="container">
     <h1>Les pays en <?php echo $continent;?> </h1>
     <h1>Le nombre de Pays : <?php echo GetNombrePays();?> </h1>
-    <h1>Le nombre de Ville : <?php echo GetNombreVille();?> </h1>
+    <h1>Le nombre de Villes : <?php echo GetNombreVille();?> </h1>
     <div>
      <table class="table">
          <tr>
@@ -46,6 +46,7 @@ $desPays = getCountriesByContinent($continent);
        <?php foreach ($desPays as $lePays){?>
           <tr>
             <td><img src="images/drapeau/<?php echo strtolower($lePays->Code2)?>.png"></td>
+            <td><a href="formulaire.php"><?php echo $lePays->Name;?></a></td>
             <td> <?php echo $lePays->Name ?></td>
             <td> <?php echo $lePays->Population ?></td>
             <td> <?php echo $lePays->Continent?></td> 
